dc2725fc00c62cc9ceb7abc02b9fb2badf2d4b39
陳乙山 <pipio1994@gmail.com>
2019-11-19 11:10:42 +0800

    Add date for tags page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -69,6 +69,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
+            date(formatString: "MMMM DD, YYYY")
             title
           }
         }
